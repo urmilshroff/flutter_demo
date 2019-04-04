efab3c0a104df64fe8ea1dd5994ad02035e6680e
urmilshroff <urmilshroff@gmail.com>
2019-04-04 21:31:18 +0530

    Fuck the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -30,43 +30,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-//class MySplashScreen extends StatefulWidget {
-//  MySplashScreen({Key key, this.title}) : super(key: key);
-//
-//  final String title; //always marked final
-//
-//  @override
-//  _MySplashScreenState createState() => _MySplashScreenState();
-//}
-//
-//class _MySplashScreenState extends State<MyHomePage> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new SplashScreen(
-//        seconds: 5,
-//        navigateAfterSeconds: new MyHomePage(),
-//        title: new Text('Change This'),
-//        image: new Image.network(
-//            'https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg'),
-//        backgroundColor: Colors.white,
-//        photoSize: 100.0,
-//        loaderColor: Colors.red);
-//  }
-//}
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-
-      // Calling setState tells Flutter that something has changed in this
-      // state and that it needs to be updated in the next build/run
-
-      // setState() also calls Widget build() below automatically
-    });
-  }
 
   void changeBrightness() {
     DynamicTheme.of(context).setBrightness(
@@ -110,3 +74,71 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+//class MySplashScreen extends StatefulWidget {
+//  MySplashScreen({Key key, this.title}) : super(key: key);
+//
+//  final String title; //always marked final
+//
+//  @override
+//  _MySplashScreenState createState() => _MySplashScreenState();
+//}
+//
+//class _MySplashScreenState extends State<MySplashScreen> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return new SplashScreen(
+//        seconds: 5,
+//        navigateAfterSeconds: new MySplashScreenState(),
+//        title: new Text('Change This'),
+//        image: new Image.asset("./flutter-logo.png"),
+//        backgroundColor: Colors.white,
+//        photoSize: 100.0,
+//        loaderColor: Colors.blue);
+//  }
+//}
+//
+//class MySplashScreenState extends MyHomePage {
+//
+//  void changeBrightness() {
+//    DynamicTheme.of(context).setBrightness(
+//        Theme.of(context).brightness == Brightness.dark
+//            ? Brightness.light
+//            : Brightness.dark);
+//  }
+//
+//  void changeColor() {
+//    DynamicTheme.of(context).setThemeData(new ThemeData(
+//        primaryColor: Theme.of(context).primaryColor == Colors.blue
+//            ? Colors.red
+//            : Colors.blue));
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    //this method is called by setState above
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("Flutter Demo"),
+//      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text('Welcome to the Flutter Demo App!',
+//                style: TextStyle(fontWeight: FontWeight.bold)),
+//            Text(
+//              'Insert dope text here',
+//              style: TextStyle(fontStyle: FontStyle.italic),
+//            ),
+//          ],
+//        ),
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: changeBrightness,
+//        tooltip: 'Light/dark mode',
+//        child: Icon(Icons.brightness_2),
+//      ),
+//    );
+//  }
+//}
